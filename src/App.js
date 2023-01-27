0a91037a139b9d617f9d5b18025aebf2bcf1fa8b
Rishabh Shukla <rishabh.shukla@acoustte.com>
2023-01-28 00:17:59 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './Components/Layout/Navbar';
+import Footer from './Components/Layout/Footer';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+
+
 
 function App() {
   return (
@@ -7,7 +13,18 @@ function App() {
      <div className="flex flex-col justify-between h-screen">
       <Navbar />
 
-      <main>Content</main>
+      <main className='container mx-auto px-3 pb-12'>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+        </Routes>
+
+
+      </main>
+
+      <Footer />
      </div>
      
     </Router>
